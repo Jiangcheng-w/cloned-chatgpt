2a9819dc6f73715923fcd1dfb38a66f3eee22e0f
Jiangcheng-w <634186866@qq.com>
2025-04-16 21:02:11 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import os
 
 from  langchain.memory import ConversationBufferMemory
 def get_chat_response(prompt, memory, openai_api_key):
-    model = ChatOpenAI(model="gpt-3.5-turbo",openai_api_base = "https://api.aigc369.com/v1",openai_api_key=openai_api_key)
+    model = ChatOpenAI(model="gpt-3.5-turbo",openai_api_key=openai_api_key)
     chain = ConversationChain(llm=model, memory=memory)
 
     response = chain.invoke({"input":prompt})
